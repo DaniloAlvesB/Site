e088aebc6d6139b5dfa07de7e240c3d15f0099a2
DaniloAlves <danilobernardino786@gmail.com>
2022-06-14 22:31:11 -0300

    Response4

--- js/header.js
+++ js/header.js
@@ -38,7 +38,17 @@ if(celular == true) {
 
             <div class="navbar-nav navbar-right d-flex flex-wrap">
 
-            <div class="btn-group">
+            </div>
+
+            <div class="navbar-nav navbar-right d-flex flex-row">
+                <a href="https://api.whatsapp.com/send?phone=5516997066673&text=Ol%C3%A1!" class="m-2"><img style="cursor: pointer;" height="40rem" src="/king_NEs/imagens/Logo/social-media-logo-collection/3x/Ativo 5@3x.png" alt=""></a>
+                <a href="" class="m-2"><img style="cursor: pointer;" height="40rem" src="/king_NEs/imagens/Logo/social-media-logo-collection/3x/Ativo 6@3x.png" alt=""></a>
+                <a href="/king_NEs/html/usuario.html" class="a_txt ml-5" style="margin-top: 5%"><img src="/king_NEs/imagens/Logo/social-media-logos-and-icons-set/3x/user.png" height="25rem"></a>
+                <a href="/king_NEs/html/Carrinho.html" target="_blank" class="a_txt mt-2 ml-3"><img src="/king_NEs/imagens/Logo/shopping-icons-collection/3x/market.png" height="25rem"> ${nmb}</a>
+            </div>
+
+        </div>
+        <div class="btn-group">
                 <a class="btn bg-realy-dark dropdown-toggle text-light" type="button" id="defaultDropdown" data-bs-toggle="dropdown" data-bs-auto-close="true" aria-expanded="false">
                 |||
                 </a>
@@ -50,17 +60,6 @@ if(celular == true) {
                 <li><a class="dropdown-item" href="/king_NEs/html/Suplementos.html">Suplementos</a></li>
                 </ul>
             </div>
-
-            </div>
-
-            <div class="navbar-nav navbar-right d-flex flex-wrap">
-                <a href="https://api.whatsapp.com/send?phone=5516997066673&text=Ol%C3%A1!" class="m-2"><img style="cursor: pointer;" height="40rem" src="/king_NEs/imagens/Logo/social-media-logo-collection/3x/Ativo 5@3x.png" alt=""></a>
-                <a href="" class="m-2"><img style="cursor: pointer;" height="40rem" src="/king_NEs/imagens/Logo/social-media-logo-collection/3x/Ativo 6@3x.png" alt=""></a>
-                <a href="/king_NEs/html/usuario.html" class="a_txt ml-5" style="margin-top: 5%"><img src="/king_NEs/imagens/Logo/social-media-logos-and-icons-set/3x/user.png" height="25rem"></a>
-                <a href="/king_NEs/html/Carrinho.html" target="_blank" class="a_txt mt-2 ml-3"><img src="/king_NEs/imagens/Logo/shopping-icons-collection/3x/market.png" height="25rem"> ${nmb}</a>
-            </div>
-
-        </div>
     `
 }else{
     console.log("PC")
