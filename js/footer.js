03d0d51d19efae01db1a3786f2d8ecdf6dd72c1c
DaniloAlves <danilobernardino786@gmail.com>
2022-06-15 08:49:23 -0300

    footer_1.4

--- js/footer.js
+++ js/footer.js
@@ -70,7 +70,7 @@ footer.innerHTML = `
 
   
     <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    <div class="text-center p-3 text-warning">
       © 2022 Copyright:
       <a class="text-white" href="#">King Nutrição Esportiva</a>
     </div>
