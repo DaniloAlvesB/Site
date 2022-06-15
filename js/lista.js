44ded767e618efb0a8c4dee568e98fce591d6eba
DaniloAlves <danilobernardino786@gmail.com>
2022-06-14 21:29:02 -0300

    config

--- js/lista.js
+++ js/lista.js
@@ -4,16 +4,16 @@
             // ['Nome', 'Descrição', valor, 'Imagem, Promo],
 
             //Peito de Frango
-            ['Peito de Frango', 'Peito de frango sem pele e sem osso  pacote de +/-1KG', 14.90, '/imagens/peitodefrango.jpg', 0],
+            ['Peito de Frango', 'Peito de frango sem pele e sem osso  pacote de +/-1KG', 14.90, '/king_NEs/imagens/peitodefrango.jpg', 0],
         
             //Coxa de Frango
-            ['Coxa de Frango', 'Coxa de Frango congelada pacote de +/-1KG', 16.90, '/imagens/teste.png', 15],
+            ['Coxa de Frango', 'Coxa de Frango congelada pacote de +/-1KG', 16.90, '/king_NEs/imagens/teste.png', 15],
         
             //Sobrecoxa de frango
-            ['Sobrecoxa de Frango', 'Sobrecoxa de Frango congelada pacote de +/-1KG', 18.90, '/imagens/teste.png', 0],
+            ['Sobrecoxa de Frango', 'Sobrecoxa de Frango congelada pacote de +/-1KG', 18.90, '/king_NEs/imagens/teste.png', 0],
 
             //Teste de frango
-            ['Teste de Frango', 'Sobrecoxa de Frango congelada pacote de +/-1KG', 20.90, '/imagens/teste.png', 19.90]
+            ['Teste de Frango', 'Sobrecoxa de Frango congelada pacote de +/-1KG', 20.90, '/king_NEs/imagens/teste.png', 19.90]
         ];
 
 
@@ -24,11 +24,11 @@
         // ['Nome', 'Descrição', valor, 'Imagem, Promo],
 
         //Peito de Frango
-        ['Wey Protein', 'Peito de frango sem pele e sem osso  pacote de +/-1KG', 14.90, '/imagens/peitodefrango.jpg', 0],
+        ['Wey Protein', 'Peito de frango sem pele e sem osso  pacote de +/-1KG', 14.90, '/king_NEs/imagens/peitodefrango.jpg', 0],
     
         //Coxa de Frango
-        ['Malto', 'Coxa de Frango congelada pacote de +/-1KG', 16.90, '/imagens/teste.png', 15],
+        ['Malto', 'Coxa de Frango congelada pacote de +/-1KG', 16.90, '/king_NEs/imagens/teste.png', 15],
     
         //Sobrecoxa de frango
-        ['Albumina', 'Sobrecoxa de Frango congelada pacote de +/-1KG', 18.90, '/imagens/teste.png', 0]
+        ['Albumina', 'Sobrecoxa de Frango congelada pacote de +/-1KG', 18.90, '/king_NEs/imagens/teste.png', 0]
     ];
